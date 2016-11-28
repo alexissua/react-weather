b2a8191858825c9015c321e8242fbc5edb080226
Alexis Suarez <alexissuarez@Alexiss-MacBook-Pro.local>
2016-11-27 22:30:33 -0500

    Style Main.jsx and Examples.jsx

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,10 +1,19 @@
 var React = require("react");
+var {Link} = require("react-router");
 
 var Examples = function(props){
 	return (
 		<div>
-	        <h3>Examples</h3>
-	        <p>Welcome to examples page!</p>
+	        <h1 className="text-center">Examples</h1>
+	        <p>Here are a few examples location to try out: </p>
+	        <ol>
+	        	<li>
+        			<Link to="/?location=Maturin">Maturin, MO</Link>
+	        	</li>
+	        	<li>
+	        		<Link to="/?location=Panama">Panama, PA</Link>
+	        	</li>
+	        </ol>
         </div>
       )
 };
--- public/bundle.js
+++ public/bundle.js
@@ -25503,11 +25503,14 @@
 			null,
 			React.createElement(Nav, null),
 			React.createElement(
-				"h2",
-				null,
-				"Main Component"
-			),
-			props.children
+				"div",
+				{ className: "row" },
+				React.createElement(
+					"div",
+					{ className: "medium-6 large-4 small-centered columns" },
+					props.children
+				)
+			)
 		);
 	};
 
@@ -27308,19 +27311,44 @@
 
 	var React = __webpack_require__(8);
 
+	var _require = __webpack_require__(166),
+	    Link = _require.Link;
+
 	var Examples = function Examples(props) {
 		return React.createElement(
 			"div",
 			null,
 			React.createElement(
-				"h3",
-				null,
+				"h1",
+				{ className: "text-center" },
 				"Examples"
 			),
 			React.createElement(
 				"p",
 				null,
-				"Welcome to examples page!"
+				"Here are a few examples location to try out: "
+			),
+			React.createElement(
+				"ol",
+				null,
+				React.createElement(
+					"li",
+					null,
+					React.createElement(
+						Link,
+						{ to: "/?location=Maturin" },
+						"Maturin, MO"
+					)
+				),
+				React.createElement(
+					"li",
+					null,
+					React.createElement(
+						Link,
+						{ to: "/?location=Panama" },
+						"Panama, PA"
+					)
+				)
 			)
 		);
 	};
